f569c7780a74e39558d9b96559f18ef4c4adec7c
sreedevi-i <72245796+sreedevi-i@users.noreply.github.com>
2021-01-18 23:13:25 -0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">THE ROCKY BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button> 
+      
+       
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              
+              <li class="nav-item">
+                  
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="tranfermoney.php">TRANSFER</a>
+              </li></ul>
+              <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          </div>
+       </nav>

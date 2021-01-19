c0ce440ac6caf68be18c6a93db2b74e772f5fb67
sreedevi-i <72245796+sreedevi-i@users.noreply.github.com>
2021-01-18 23:33:58 -0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>ABOUT</title>
+ <style>
+     body
+     {
+        background-color: black; 
+       
+     }
+ </style>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <h1><u>ABOUT</u></h1>
+  <h2>This is a project for THE SPARKS FOUNDATION.</h2>
+  <h3><p> The Sparks Foundtion Web Development and designing internship project: Basic Banking System A Web Application used to transfer money between multiple users and also record the transaction activities.<br>
+
+Languages used: HTML, PHP, JAVASCRIPT , CSS<br>
+
+The Website has the following specifications:<br>
+
+1. A dummy data for 10 customers.<br>
+2. Customers table with basic fields such as name ,email, current balance etc.<br>
+3. Transaction history which records all Transactions.<br>
+
+Website flow is : Home page> View Customers> Transfer Money> View Transaction History.</p>
+<br>
+Follow The Sparks Foundation On LINKEDIN <a href="https://www.linkedin.com/in/the-sparks-foundation/" >HERE</a></h3>
+</body>
+</html>

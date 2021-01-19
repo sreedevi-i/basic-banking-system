6718476271965bb122db3761dd2ed2a610995d51
sreedevi-i <72245796+sreedevi-i@users.noreply.github.com>
2021-01-18 23:11:36 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+     
+
+    <title>Basic Banking System</title>
+ <style>
+     body
+     {
+        background-color: black; 
+       
+     }
+     
+   
+    
+     
+ </style>
+  </head>
+
+  <body>
+      
+  <?php
+  include 'navbar.php';
+  ?>
+  
+     
+      
+    
+
+<div class="main">
+
+         
+      <div class="container-fluid">
+      
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                 <h3 style="text-align:center"> Welcome to</h3><h1> THE ROCKY BANK</h1>
+                 </div>
+                 <marquee><div><h2>Transferring money made simple and secure at your fingertips.</h2>
+                </div></marquee>
+              </div>
+              
+              
+              
+              <div class="container-fluid hero-content">
+      <div class="row align-items-center">
+              
+            <div class="column activity text-center">
+                  <div class="col-md act">
+                   
+                    <img src="img/sendmoney.png" class="img-fluid">
+                    <a href="tranfermoney.php"><button>View Customers to transfer</button></a>
+                  </div>
+                  
+            </div>
+         
+     </div>
+           
+    
+     
+     <div id="placebelow" style="text-align:center">
+       <h4> <p style="color:##f2f2f2">WEB DEVELOPMENT INTERN TASK 1 <b>BASIC BANKING SYSTEM</b> <br>for The Sparks Foundation<br>DESIGN BY:<b>I. SREE DEVI</b></p>
+      </h4>
+     </div></div></div>
+
+    <nav class= "navbar navbar-expand-md navbar-light bg-light">
+     <a class="navbar-brand" href="index.php">THE ROCKY BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">ABOUT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/shree-devi-i">LINKEDIN</a>
+              </li></ul>
+              
+          </div>
+       </nav>
+        
+ 
+     <script src="script.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
